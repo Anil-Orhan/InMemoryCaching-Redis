6ca372bc7f6b61924c2aa0f96cc030e0b0075c6c
anilorhan <118168321+anilunilogistix@users.noreply.github.com>
2023-03-24 20:43:56 +0300

    First Commit

---
+++ RedisInMemory.Web/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace RedisInMemory.Web.Controllers;
+
+public class ProductController : Controller
+{
+    private IMemoryCache _memoryCache;
+    public ProductController(IMemoryCache memoryCache)
+    {
+        _memoryCache = memoryCache;
+    }
+    // GET
+    public IActionResult Index()
+    {
+        MemoryCacheEntryOptions options = new MemoryCacheEntryOptions();
+        options.AbsoluteExpiration=DateTime.Now.AddSeconds(20);
+        
+       // options.Priority = CacheItemPriority.Low; //Düşük Öncelikli Cache
+       // options.Priority = CacheItemPriority.Normal; //Normal Öncelikli Cache
+        options.Priority = CacheItemPriority.High;//Yüksek Öncelikli Cache
+       // options.Priority = CacheItemPriority.NeverRemove; //Ram'den asla silinmeyecek Cache
+        
+        _memoryCache.Set<string>("time",DateTime.Now.ToString(),options);
+        
+        return View();
+    }
+
+    public IActionResult Show()
+    {
+        _memoryCache.TryGetValue("time", out string timecache);
+       
+       
+       ViewBag.time= timecache;
+       return View();
+    }
+}
