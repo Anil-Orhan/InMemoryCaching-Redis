287bdfc45a1bc1e4767a39a511338293dddcdf41
Anil-Orhan <anilorhanmail@gmail.com>
2023-03-28 21:08:59 +0300

    ByteSerilaize ok

--- RedisIDistributedCacheApp.Web/Controllers/ProductsController.cs
+++ RedisIDistributedCacheApp.Web/Controllers/ProductsController.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Data;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using RedisIDistributedCacheApp.Web.Models;
 
 namespace RedisIDistributedCacheApp.Web.Controllers;
 
@@ -10,12 +16,33 @@ public class ProductsController : Controller
     public ProductsController(IDistributedCache distributedCache)
     {
         _distributedCache = distributedCache;
-        _distributedCache.
+        
     }
     
-    // GET
-    public IActionResult Index()
+    // Set
+    public async Task<IActionResult> Index()
     {
+        DistributedCacheEntryOptions options = new DistributedCacheEntryOptions();
+        options.AbsoluteExpiration=DateTimeOffset.Now.AddMinutes(1);
+
+        Product product = new Product() {id = 1,name = "Kalem",price = (decimal)100.5,stock = 250};
+        string jsonproduct = JsonConvert.SerializeObject(product);
+
+        Byte[] byteproduct = Encoding.UTF8.GetBytes(jsonproduct);
+         _distributedCache.Set("product:1",byteproduct);
+      // await _distributedCache.SetStringAsync("product:1", jsonproduct, options);
+        
+        return View();
+    }
+    
+    //Get
+    
+    public IActionResult Show()
+    {
+      
+       var cache=  _distributedCache.Get("product:1");
+      // Product product = JsonConvert.DeserializeObject<Product>(cache);
+       ViewBag.cache = cache;
         return View();
     }
 }
---
+++ RedisIDistributedCacheApp.Web/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace RedisIDistributedCacheApp.Web.Models;
+
+public class Product
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public int stock { get; set; }
+    public decimal price { get; set; }
+}
