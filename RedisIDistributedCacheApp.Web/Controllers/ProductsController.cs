22a2c76829b0cb9b711233a04581f553d5ccf25e
anilorhan <anilorhanmail@gmail.com>
2023-03-28 18:39:09 +0300

    Redis New Project Update

---
+++ RedisIDistributedCacheApp.Web/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace RedisIDistributedCacheApp.Web.Controllers;
+
+public class ProductsController : Controller
+{
+    private readonly IDistributedCache _distributedCache;
+    
+    public ProductsController(IDistributedCache distributedCache)
+    {
+        _distributedCache = distributedCache;
+        _distributedCache.
+    }
+    
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
